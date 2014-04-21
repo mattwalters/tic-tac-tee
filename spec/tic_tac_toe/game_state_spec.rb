2a365fe1d9dce7f6e39cc8223251089bfe9189f5
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 10:38:38 -0700

    add spec to check that each element == :empty

--- spec/tic_tac_toe/game_state_spec.rb
+++ spec/tic_tac_toe/game_state_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe TicTacToe::GameState do
-  let(:game_state) { TicTacToe::GameState.new }
-  it "responds with a 9 element array to #current_state" do
-    expect(game_state.current_state.size).to eq(9)
+  context "when game_state is still in initial state" do 
+    let(:game_state) { TicTacToe::GameState.new }
 
+    it "responds with a 9 element array to #current_state" do
+      expect(game_state.current_state.size).to eq(9)
+    end
 
+    it "puts :empty for each of the 9 elements" do
+      expect(game_state.current_state.all? { |space| space == :empty }).to be_true
+    end
   end
 end
 
