eef0719f601b841e3f9a906d21c3a09f096b10f4
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 15:12:42 -0700

    passing tests

--- lib/tic_tac_toe/game_state.rb
+++ lib/tic_tac_toe/game_state.rb
@@ -11,6 +11,8 @@ module TicTacToe
       end
     end
 
+
+
     def current_state
       state
     end
@@ -40,13 +42,30 @@ module TicTacToe
       state[x][y] == :nought
     end
     def terminal_state?
-      false
+      terminal_space_sets.any? do |space_set|
+	space_set.all? do |x, y|
+	  cross?(x, y) || nought?(x, y)
+	end
+      end
     end
     def winner
       nil
     end
 
     private 
+
+    def terminal_columns
+      (0...3).map { |x| (0...3).map { |y| [ x, y ] } }
+    end
+    def terminal_rows
+      (0...3).map { |y| (0...3).map { |x| [ x, y ] } }
+    end
+    def terminal_diagonals
+      [ (0...3).map { |n| [ n, n ] }, (0...3).map { |n| [ 2 - n, n ] } ]
+    end
+    def terminal_space_sets
+      terminal_columns + terminal_rows + terminal_diagonals
+    end
 
     def validate_in_bounds!(x, y)
       unless x.in?(0...3) && y.in?(0..3)
@@ -55,7 +74,7 @@ module TicTacToe
     end
     def validate_empty!(x, y)
       unless empty?(x, y)
-        raise SpaceNotVacantError, "(#{x}, #{y}) was #{state[x][y]} not empty" 
+	raise SpaceNotVacantError, "(#{x}, #{y}) was #{state[x][y]} not empty" 
       end
     end
 
