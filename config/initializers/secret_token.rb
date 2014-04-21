a28e89398a0a52816bb92cdf16531f6e681c132d
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 09:15:12 -0700

    primordial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicTacTee::Application.config.secret_key_base = '622e60dac2adc4d12b52479c6a379b2767d576d7b182a9f1ba1771508a4c81874987c0acf71fff8eb0c100c6cdbe8a98b8d37d19dd888f705d75fc21e54845e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TicTacTee::Application.config.session_store :cookie_store, key: '_tic_tac_tee_session'
