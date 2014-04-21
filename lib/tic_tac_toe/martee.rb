9883768c48fcc4826a8e41e0cdb500eb60c81b2e
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 16:55:34 -0700

    passing tests

--- lib/tic_tac_toe/martee.rb
+++ lib/tic_tac_toe/martee.rb
@@ -1,6 +1,13 @@
 module TicTacToe
   class Martee
 
+    def move!(game_state)
+      if game_state.empty?(1, 1)
+	game_state.nought!(1, 1)
+      else
+	game_state.nought!(0, 0)
+      end
+    end
 
   end
 end
