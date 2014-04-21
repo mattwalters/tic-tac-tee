b42973ac470685453e75033a355c8a34446c8e87
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 14:12:54 -0700

    passing specs

--- lib/tic_tac_toe/game_state.rb
+++ lib/tic_tac_toe/game_state.rb
@@ -1,5 +1,6 @@
 module TicTacToe
   class SpaceNotVacantError < StandardError; end
+  class IllegalSpaceError < StandardError; end
   class GameState
 
     def initialize
@@ -15,26 +16,27 @@ module TicTacToe
     end
 
     def cross!(x, y)
-      unless empty?(x, y)
-        raise SpaceNotVacantError, "(#{x}, #{y}) was #{state[x][y]} not empty" 
-      end
+      validate_empty!(x, y)
+      validate_in_bounds!(x, y)
       state[x][y] = :cross
     end
 
     def nought!(x, y)
-      unless empty?(x, y)
-        raise SpaceNotVacantError, "(#{x}, #{y}) was #{state[x][y]} not empty" 
-      end
+      validate_empty!(x, y)
+      validate_in_bounds!(x, y)
       state[x][y] = :nought
     end
 
     def empty?(x, y)
+      validate_in_bounds!(x, y)
       state[x][y] == :empty
     end
     def cross?(x, y)
+      validate_in_bounds!(x, y)
       state[x][y] == :cross
     end
     def nought?(x, y)
+      validate_in_bounds!(x, y)
       state[x][y] == :nought
     end
     def terminal_state?
@@ -45,6 +47,17 @@ module TicTacToe
     end
 
     private 
+
+    def validate_in_bounds!(x, y)
+      unless x.in?(0...3) && y.in?(0..3)
+	raise IllegalSpaceError, "illegal space: (#{x}, #{y})"
+      end
+    end
+    def validate_empty!(x, y)
+      unless empty?(x, y)
+        raise SpaceNotVacantError, "(#{x}, #{y}) was #{state[x][y]} not empty" 
+      end
+    end
 
     attr_reader :state
   end
