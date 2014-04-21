99d5f149489be21a5bfb1f55fc9cba918b8a45d1
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 12:42:21 -0700

    passing tests

--- lib/tic_tac_toe/game_state.rb
+++ lib/tic_tac_toe/game_state.rb
@@ -1,5 +1,7 @@
 module TicTacToe
+  class SpaceNotVacantError < StandardError; end
   class GameState
+
 
     def initialize
       @state = (0...3).to_a.map do 
@@ -14,11 +16,14 @@ module TicTacToe
     end
 
     def cross!(x, y)
+      unless empty?(x, y)
+        raise SpaceNotVacantError, "(#{x}, #{y}) was #{state[x][y]} not empty" 
+      end
       state[x][y] = :cross
     end
 
     def empty?(x, y)
-      true
+      state[x][y] == :empty
     end
     def cross?(x, y)
       state[x][y] == :cross
