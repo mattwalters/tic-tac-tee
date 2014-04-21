8a4e4ff9f290059d7c280c01b18a8d598096b14b
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 13:56:27 -0700

    refactor spec

--- spec/tic_tac_toe/game_state_spec.rb
+++ spec/tic_tac_toe/game_state_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-describe TicTacToe::GameState do
 
-  def three_by_three_product
-    (0...3).to_a.product((0...3).to_a)
-  end
+# I think these constants should be helpers
+# but I had some trouble getting them to work quickly
+# this seems to work for now -- would love some pointers
+# on how best to resue code in specs :)
+ALL_SPACES = (0...3).to_a.product((0...3).to_a)
+MARKERS = [ :cross, :nought ]
+
+describe TicTacToe::GameState do
 
   context "when game_state is still in initial state" do 
     let(:game_state) { TicTacToe::GameState.new }
@@ -13,33 +17,9 @@ describe TicTacToe::GameState do
       expect(game_state.current_state.size).to eq(3)
       3.times { |n| expect(game_state.current_state[n].size).to eq(3) }
     end
-
     it "responds true to all empty?(x, y)" do
-      three_by_three_product.each do |x, y|
-        expect(game_state.empty?(x, y)).to be_true
-      end
-    end
-
-    it "responds false to all cross?(x, y)" do
-      three_by_three_product.each do |x, y|
-        expect(game_state.cross?(x, y)).to be_false
-      end
-    end
-    it "can cross!(x, y) for all x,y pairs" do 
-      three_by_three_product.each do |x, y|
-	game_state.cross!(x, y)
-	expect(game_state.cross?(x, y)).to be_true
-      end
-    end
-    it "responds false to all nought?(x, y)" do
-      three_by_three_product.each do |x, y|
-        expect(game_state.nought?(x, y)).to be_false
-      end
-    end
-    it "can nought!(x, y) for all x,y pairs" do 
-      three_by_three_product.each do |x, y|
-	game_state.nought!(x, y)
-	expect(game_state.nought?(x, y)).to be_true
+      ALL_SPACES.each do |x, y|
+	expect(game_state.empty?(x, y)).to be_true
       end
     end
     it "responds false to terminal_state?" do
@@ -48,26 +28,38 @@ describe TicTacToe::GameState do
     it "it responds nil to winner" do 
       expect(game_state.winner).to be_nil
     end
-    context "when cross moves 1x1" do
-      let(:game_state) do 
-	game_state = TicTacToe::GameState.new
-	game_state.cross!(1, 1)
-	game_state
-      end	
-      it "responds true to #cross?(1, 1)" do
-	expect(game_state.cross?(1, 1)).to be_true
-      end
-      it "responds false to everything but #cross?(1, 1)" do 
-	three_by_three_product.each do |x, y|
-	  expect(game_state.cross?(x, y)).to be_false unless [ x, y ] == [ 1, 1 ]
+
+    MARKERS.each do |marker|
+      it "responds false to all #{marker}?(x, y)" do
+	ALL_SPACES.each do |x, y|
+	  expect(game_state.send("#{marker}?", x, y)).to be_false
 	end
       end
-      it "raises an error if moving to a non-empty space" do
-	expect { game_state.cross!(1, 1) }.to raise_error(TicTacToe::SpaceNotVacantError)
+      it "can #{marker}!(x, y) for all x,y pairs" do 
+	ALL_SPACES.each do |x, y|
+	  game_state.send("#{marker}!", x, y)
+	  expect(game_state.send("#{marker}?", x, y)).to be_true
+	end
+      end
+
+      ALL_SPACES.each do |x, y|
+
+	context "when #{marker}!(#{x}x#{y})" do
+	  let(:game_state) do 
+	    game_state = TicTacToe::GameState.new
+	    game_state.send("#{marker}!", x, y)
+	    game_state
+	  end	
+	  it "responds true to ##{marker}?(#{x}, #{y})" do
+	    expect(game_state.send("#{marker}?", x, y)).to be_true
+	  end
+	  it "raises an error if marking to a non-empty space" do
+	    expect { game_state.send("#{marker}!", x, y) }.to raise_error(
+	      TicTacToe::SpaceNotVacantError
+	    )
+	  end
+	end
       end
     end
   end
-
 end
-
-
