ebb704a087b17b9ade873b47df5c59af514c35c6
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 17:15:03 -0700

    back up a step

--- lib/tic_tac_toe/martee.rb
+++ lib/tic_tac_toe/martee.rb
@@ -1,13 +1,6 @@
 module TicTacToe
   class Martee
 
-    def move!(game_state)
-      if game_state.empty?(1, 1)
-	game_state.nought!(1, 1)
-      else
-	game_state.nought!(0, 0)
-      end
-    end
 
   end
 end
