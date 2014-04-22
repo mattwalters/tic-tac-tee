ebebe3d42187b76354b06d90169a53f5e219429a
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 18:10:07 -0700

    spec for martee to win if possible

--- lib/tic_tac_toe/martee.rb
+++ lib/tic_tac_toe/martee.rb
@@ -1,6 +1,32 @@
 module TicTacToe
   class Martee
 
+    def initialize(game_state)
+      @game_state = game_state
+    end
+
+    def move!
+      win! if can_win?
+    end
+
+    def can_win?
+      game_state.terminal_space_sets.any? do |space_set|
+	space_set.count { |x, y| game_state.nought?(x, y) } == 2 && 
+	  space_set.one? { |x, y| game_state.empty?(x, y) }
+      end
+    end
+    def win!
+      game_state.terminal_space_sets.each do |space_set|
+	if space_set.count { |x, y| game_state.nought?(x, y) } == 2 && 
+	  space_set.one? { |x, y| game_state.empty?(x, y) }
+	  space = space_set.find { |space| game_state.empty?(*space) }
+	  game_state.nought!(*space)
+	  break
+	end
+      end
+    end
+
+    attr_reader :game_state
 
   end
 end
--- spec/tic_tac_toe/martee_spec.rb
+++ spec/tic_tac_toe/martee_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper'
 
 describe TicTacToe::Martee do
-  let(:martee) { TicTacToe::Martee.new }
 
-  context "when martee has a winning move" do
-    let(:game_state) { TicTacToe::GameState.new }
-
-    TERMINAL_SPACE_SETS.each do |space_set|
-      3.times do |n|
-	it "wins" do 
+  TERMINAL_SPACE_SETS.each do |space_set|
+    3.times do |n|
+      context "when martee has a winning move" do
+	let(:game_state) do 
+	  game_state = TicTacToe::GameState.new
 	  space_set.each_with_index do |(x, y), i|
 	    game_state.nought!(x, y) unless n == i
 	  end
-	  expect(game_state.winner).to be_nil
-	  martee.move!(game_state)
+	  game_state
+	end
+	let(:martee) { TicTacToe::Martee.new(game_state) }
+	
+	it "wins" do 
+	  martee.move!
 	  expect(game_state.winner).to eq(:nought)
 	end
       end
