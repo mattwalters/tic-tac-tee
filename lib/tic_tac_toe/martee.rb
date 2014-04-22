1f69ff7c5bc7e7569645bb805e6381e93f1a329c
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 20:58:09 -0700

    fork and block_fork is pretty bad.... might come back to it

--- lib/tic_tac_toe/martee.rb
+++ lib/tic_tac_toe/martee.rb
@@ -6,8 +6,88 @@ module TicTacToe
     end
 
     def move!
-      win! if can_win?
-      block! if can_block?
+      if can_win?
+	win!
+      elsif can_block?
+	block!
+      elsif can_fork?
+	fork!
+      end
+    end
+
+    def can_block_fork?
+      game_state.terminal_space_sets.each do |outer_space_set|
+	game_state.terminal_space_sets.each do |inner_space_set|
+	  next if outer_space_set == inner_space_set
+	  if outer_space_set.one? { |space| (space.in? inner_space_set) && 
+			     game_state.empty?(*space) }
+	    if outer_space_set.one? { |space| game_state.cross?(*space) } &&
+	      inner_space_set.one? { |space| game_state.cross?(*space) } &&
+	      outer_space_set.count { |space| game_state.empty?(*space) } == 2 &&
+	      inner_space_set.count { |space| game_state.empty?(*space) } == 2
+	      return true
+	    end
+	  end
+	end
+      end
+      false
+    end
+    def block_fork!
+      game_state.terminal_space_sets.each do |outer_space_set|
+	game_state.terminal_space_sets.each do |inner_space_set|
+	  if outer_space_set.one? { |space| (space.in? inner_space_set) && 
+			     game_state.empty?(*space) }
+	    if outer_space_set.one? { |space| game_state.cross?(*space) } &&
+	      inner_space_set.one? { |space| game_state.cross?(*space) } &&
+	      outer_space_set.count { |space| game_state.empty?(*space) } == 2 &&
+	      inner_space_set.count { |space| game_state.empty?(*space) } == 2
+	      # find the shared empty space
+	      target_space = outer_space_set.find { |space| (space.in? inner_space_set) &&
+					     game_state.empty?(*space) }
+	      game_state.nought!(*target_space)
+	      return
+	    end
+	  end
+	end
+      end
+
+    end
+    def can_fork?
+      game_state.terminal_space_sets.each do |outer_space_set|
+	game_state.terminal_space_sets.each do |inner_space_set|
+	  next if outer_space_set == inner_space_set
+	  if outer_space_set.one? { |space| (space.in? inner_space_set) && 
+			     game_state.empty?(*space) }
+	    if outer_space_set.one? { |space| game_state.nought?(*space) } &&
+	      inner_space_set.one? { |space| game_state.nought?(*space) } &&
+	      outer_space_set.count { |space| game_state.empty?(*space) } == 2 &&
+	      inner_space_set.count { |space| game_state.empty?(*space) } == 2
+	      return true
+	    end
+	  end
+	end
+      end
+      false
+    end
+
+    def fork!
+      game_state.terminal_space_sets.each do |outer_space_set|
+	game_state.terminal_space_sets.each do |inner_space_set|
+	  if outer_space_set.one? { |space| (space.in? inner_space_set) && 
+			     game_state.empty?(*space) }
+	    if outer_space_set.one? { |space| game_state.nought?(*space) } &&
+	      inner_space_set.one? { |space| game_state.nought?(*space) } &&
+	      outer_space_set.count { |space| game_state.empty?(*space) } == 2 &&
+	      inner_space_set.count { |space| game_state.empty?(*space) } == 2
+	      # find the shared empty space
+	      target_space = outer_space_set.find { |space| (space.in? inner_space_set) &&
+					     game_state.empty?(*space) }
+	      game_state.nought!(*target_space)
+	      return
+	    end
+	  end
+	end
+      end
     end
 
     def can_block?
@@ -26,7 +106,6 @@ module TicTacToe
 	  break
 	end
       end
-
     end
 
     def can_win?
@@ -35,6 +114,7 @@ module TicTacToe
 	  space_set.one? { |x, y| game_state.empty?(x, y) }
       end
     end
+
     def win!
       game_state.terminal_space_sets.each do |space_set|
 	if space_set.count { |x, y| game_state.nought?(x, y) } == 2 && 
--- spec/tic_tac_toe/martee_spec.rb
+++ spec/tic_tac_toe/martee_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe TicTacToe::Martee do
+describe TicTacToe::Martee do 
 
   TERMINAL_SPACE_SETS.each do |space_set|
     3.times do |n|
@@ -13,9 +13,9 @@ describe TicTacToe::Martee do
 	  game_state
 	end
 	let(:martee) { TicTacToe::Martee.new(game_state) }
-	
+
 	it "wins" do 
-	  martee.move!
+	  martee.win!
 	  expect(game_state.winner).to eq(:nought)
 	end
       end
@@ -33,9 +33,9 @@ describe TicTacToe::Martee do
 	  game_state
 	end
 	let(:martee) { TicTacToe::Martee.new(game_state) }
-	
+
 	it "blocks" do 
-	  martee.move!  
+	  martee.block!  
 	  expect(space_set.count { |space| game_state.cross?(*space) }).to eq(2)
 	  expect(space_set.one? { |space| game_state.nought?(*space) }).to be_true
 	end
@@ -43,6 +43,58 @@ describe TicTacToe::Martee do
     end
   end
 
+  TERMINAL_SPACE_SETS.each do |outer_space_set|
+    TERMINAL_SPACE_SETS.each do |inner_space_set|
+      unless outer_space_set == inner_space_set
+	if outer_space_set.one? { |space| space.in? inner_space_set }
+	  context "when martee can fork" do
+	    let(:game_state) do
+	      game_state = TicTacToe::GameState.new
+	      outer_space = outer_space_set.find { |space| !space.in? inner_space_set }
+	      inner_space = inner_space_set.find { |space| !space.in? outer_space_set }
+	      game_state.nought!(*outer_space)
+	      game_state.nought!(*inner_space)
+	      game_state
+	    end
+	    let(:martee) { TicTacToe::Martee.new(game_state) }
+
+	    it "forks" do 
+	      expect(martee.can_fork?).to be_true
+	      # having a hard time getting this right...
+	    end
+	  end
+	end
+      end
+    end
+  end
+
+
+  TERMINAL_SPACE_SETS.each do |outer_space_set|
+    TERMINAL_SPACE_SETS.each do |inner_space_set|
+      unless outer_space_set == inner_space_set
+	if outer_space_set.one? { |space| space.in? inner_space_set }
+	  context "when martee can block a fork" do
+	    let(:game_state) do
+	      game_state = TicTacToe::GameState.new
+	      outer_space = outer_space_set.find { |space| !space.in? inner_space_set }
+	      inner_space = inner_space_set.find { |space| !space.in? outer_space_set }
+	      game_state.cross!(*outer_space)
+	      game_state.cross!(*inner_space)
+	      game_state
+	    end
+	    let(:martee) { TicTacToe::Martee.new(game_state) }
+
+	    it "block fork" do 
+	      expect(martee.can_block_fork?).to be_true
+	      # having a hard time getting this right...
+	    end
+	  end
+	end
+      end
+    end
+  end
+
+
+
 
 end
-
