7641dab4c4837a24051b385ba97c2e6bd67d37a2
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 18:20:45 -0700

    add spec for martee to block oppenent if able

--- spec/tic_tac_toe/martee_spec.rb
+++ spec/tic_tac_toe/martee_spec.rb
@@ -21,5 +21,28 @@ describe TicTacToe::Martee do
       end
     end
   end
+
+  TERMINAL_SPACE_SETS.each do |space_set|
+    3.times do |n|
+      context "when opponent has a winning move" do
+	let(:game_state) do 
+	  game_state = TicTacToe::GameState.new
+	  space_set.each_with_index do |(x, y), i|
+	    game_state.cross!(x, y) unless n == i
+	  end
+	  game_state
+	end
+	let(:martee) { TicTacToe::Martee.new(game_state) }
+	
+	it "blocks" do 
+	  martee.move!  
+	  expect(space_set.count { |space| game_state.cross?(*space) }).to eq(2)
+	  expect(space_set.one? { |space| game_state.nought?(*space) }).to be_true
+	end
+      end
+    end
+  end
+
+
 end
 
