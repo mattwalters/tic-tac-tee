217b80bae592c5cb6856bb4f521289a4090ba21c
mattwalters <matthewrobertwalters@gmail.com>
2014-04-21 18:21:05 -0700

    passing tests

--- lib/tic_tac_toe/martee.rb
+++ lib/tic_tac_toe/martee.rb
@@ -7,6 +7,26 @@ module TicTacToe
 
     def move!
       win! if can_win?
+      block! if can_block?
+    end
+
+    def can_block?
+      game_state.terminal_space_sets.any? do |space_set|
+	space_set.count { |x, y| game_state.cross?(x, y) } == 2 && 
+	  space_set.one? { |x, y| game_state.empty?(x, y) }
+      end
+    end
+
+    def block!
+      game_state.terminal_space_sets.each do |space_set|
+	if space_set.count { |x, y| game_state.cross?(x, y) } == 2 && 
+	  space_set.one? { |x, y| game_state.empty?(x, y) }
+	  space = space_set.find { |space| game_state.empty?(*space) }
+	  game_state.nought!(*space)
+	  break
+	end
+      end
+
     end
 
     def can_win?
